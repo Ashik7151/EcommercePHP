b4312ac1f0859024e4df034032894dabbb18bf16
Ashik <107695832+Ashik7151@users.noreply.github.com>
2023-02-17 14:28:53 +0600

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Omar</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Ashik</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       
